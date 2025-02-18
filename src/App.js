7ad2a7fedd41c1cd503e83821ef847ecad38fa1e
AyushPawar004 <ayush.pawar@mosaicwellness.in>
2025-02-18 11:21:36 +0530

    google analytics added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import RestaurantsList from "./RestaurantsList.js";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-T418MD1TGJ"); // Replace with your Measurement ID
 
 function App() {
+  ReactGA.send("pageview");
   return (
     <div>
       <RestaurantsList />
